2b268772ebc93213a0cd9bbf8fc91767d503794d
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2023-09-13 20:53:03 +0530

    Update Navigation.js
    Context conumer

--- components/MainHeader/Navigation.js
+++ components/MainHeader/Navigation.js
@@ -1,28 +1,37 @@
 import React from 'react';
 
 import classes from './Navigation.module.css';
+import UserContext from '../Context/auth.context';
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
+    <UserContext.Consumer>
+      {
+        (ctx)=>{
+          return (
+            <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
         )}
       </ul>
     </nav>
+          )
+        }
+      }
+      </UserContext.Consumer>
   );
 };
 
